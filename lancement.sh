417cd45bd2b7d2e0cef7062a70aff53119d09f1f
SWGAKamui <kamui_mangas@hotmail.com>
2015-06-30 14:39:04 +0200

    update

--- lancement.sh
+++ lancement.sh
@@ -13,37 +13,24 @@ if [  $# -eq 0 ] || [ "$EXT" != "metablocks" ]
       #display_usage
       exit 1
 else
-
    script/./modif.sh "$1"
-
-
    wait
-
    make
    function/./main txt/"$NAME.txt"
-
    wait
    make clean
-
-
    wait
    if [ ! -e /dev/ttyACM0 ]
    then
    	echo  "\n=======! No device !=======\n"
-
    else
    	echo "start" > /dev/ttyACM0
-
    	while read line  
    	do   
       		echo "$line" > /dev/ttyACM0
       		sleep 100
       		wait
-      		wait
-      		wait
-      		wait
       		echo "stop" > /dev/ttyACM0
    	done < test.txt
    fi
-
 fi
